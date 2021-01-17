3b6a1dea279553aefbca11ec1aa026e099376dac
Aryanakh7 <Aryanakh7@users.noreply.github.com>
2021-01-16 21:08:58 -0500

    Election Analysis - Final Results

---
+++ Python_Practice.py
@@ -0,0 +1,128 @@
+# counties = ["Arapahoe", "Denver", "Jefferson"]
+# if counties[1] == 'Denver':
+#     print(counties[1])
+
+# if counties[3] != 'Jefferson'
+#     print(counties[2])
+
+# temperature = int(input("What is the temperature outside? "))
+
+# if temperature > 80:
+#     print("Turn on the AC.")
+# else:
+#     print("Open the windows.")
+
+# What is the score?
+# What is the score ? 
+# score = int(input("What is your test score? "))
+
+# Determine the grade 
+# if score >= 90:
+#     print('Your grade is an A.')
+# elif score >= 80:
+#     print('Your grade is a B.')
+# elif score >= 70:
+#     print('Your grade is a C.')
+# elif score >= 60:
+#     print('Your grade is a D.')
+# else:
+#     print('Your grade is an F.')
+
+# counties = ["Arapahoe", "Denver", "Jefferson"]
+# if "El Paso" in counties:
+#     print("El Paso is in the list of counties. ")
+# else:
+#     print("El Paso is not the list of counties")
+
+# x = 5
+# y = 5
+# if x == 5 and y == 5:
+#     print("True")
+# else:
+#     print("False")
+
+# if "Arapahoe" in counties and "El Paso" not in counties:
+#     print("Only Arapahoe is in the of counties.")
+# else:
+#     print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
+
+# x = 0 
+# while x <= 5:
+#     print(x)
+#     x= x + 1
+
+# count = 7 
+# while count < 0:
+#     print("Hello World")
+
+# for county in counties:
+#     print('Jefferson')
+
+# numbers = [0, 1, 2, 3, 4]
+# for num in numbers:
+#     print(num)
+
+# for num in range(5):
+#     print (num)
+
+# for i in range(len(counties)):
+#     print(counties[i])
+
+# counties_tuple = ("Arapahoe", "Denver", "Jefferson").
+
+# Arapahoe
+# Denver
+# Jefferson
+
+# print("Hello World")
+
+# print("Your interest for the year is $" + str(3000))
+
+# my_votes = int(input("How many votes did you get in the election? "))
+# total_votes = int(input("What is the total votes in the election? "))
+# print(f"I received {my_votes / total_votes * 100} of the total votes.")
+
+import csv
+dir (csv)
+
+# election_data = open(C:\\Users\\Aryana Kounou\\Desktop\\Resources\\election_results.csv")
+
+# file_to_load = os.path.join( "Users" , "Aryana Kounou", "Desktop", "Resources", "election_results.csv")
+# With open(file_to_load) as election_data:
+#     print(election_data)
+
+# file_to_load = ("C:\Users\Aryana Kounou\Desktop\Resources\election_results.csv")
+
+# Add our dependencies.
+import csv
+import os
+
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+with open(file_to_load) as election_data:
+    
+# print (election_data)
+
+
+# Open the election results and read the file.
+
+# # Using the with statement open the file as a text file.
+# with open(file_to_save, "w") as txt_file:
+
+#       # Write three counties to the file.
+#      txt_file.write("Arapahoe\nDenver\nJefferson")
+ 
+# To do: read and analyze the data here.
+
+# Read the file object with the reader function.
+csv_reader = csv.reader(election_data)
+ # Print each row in the CSV file.
+    for row in file_reader:
+        print(row)
+
+
+
